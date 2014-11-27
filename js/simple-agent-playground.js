4432611436a48088126f89a718d92d611eacdeb2
Sébastien Barbieri <seba@rtbf.be>
2014-11-27 15:35:12 +0100

    Second function added

--- js/simple-agent-playground.js
+++ js/simple-agent-playground.js
@@ -7,46 +7,52 @@
     new View(document.getElementById('playground'), problem);
 
     var functionApproximator = new FunctionApproximator(
-	function(){return Math.random();},
-	0.001,
-	0.01,
-	function(elements) { return elements[Math.floor((Math.random() * elements.length))]; },
-	function(weights) {
-	    return 1/weights.reduce(function (prev, current) { return Math.max(prev, current)}, -Infinity)
-	}
+    function(){return Math.random();},
+    0.001,
+    0.01,
+    function(elements) { return elements[Math.floor((Math.random() * elements.length))]; },
+    function(weights) {
+        return 1/weights.reduce(function (prev, current) { return Math.max(prev, current)}, -Infinity)
+    }
     );
 
     functionApproximator.addValueFunction(
-	functionApproximator.createValueFunction(function(s) {
-	    return 1;
-	})
+        functionApproximator.createValueFunction(function(s) {
+            return 1;
+        })
+    );
+    functionApproximator.addValueFunction(
+        functionApproximator.createValueFunction(function(s) {
+            console.log(s);
+            return -10000.0*Math.abs(s.angle);
+        })
     );
 
     var agent = new SimpleAgent(problem, functionApproximator);
     var count = 0;
+    var framecount = 0;
     function run(){
-	if(!problem.currentState().ended) {
-	    if(count%5 == 0) {
-		var option = agent.chooseAction();
+        if (framecount % 1 == 0){
+            if(!problem.currentState().ended) {
+                if(count%5 == 0) {
+                var option = agent.chooseAction();
 
-		agent.performAction(option.action);
-	    } else {
-		problem.tick();
-	    }
-	    count++;
-	} else {
-
-	    agent.reevaluateActions();
-
-	    var log = "";
-	    agent.solver.getValueFunctions().forEach(function(el) { log += " " +  el.getWeight(); } );
-	    //log weights
-	    //console.log(log);
-
-	    problem.currentState(initialState);
-
-	}
-	requestAnimationFrame(run);
+                agent.performAction(option.action);
+                } else {
+                problem.tick();
+                }
+                count++;
+            } else {
+                agent.reevaluateActions();
+                var log = "";
+                agent.solver.getValueFunctions().forEach(function(el) { log += " " +  el.getWeight(); } );
+                //log weights
+                //console.log(log);
+                problem.currentState(initialState);
+            }
+        }
+        framecount++;
+        requestAnimationFrame(run);
     };
     run();
 })(World, InvertedPendulumView, FunctionApproximator, SimpleAgent);
